89f09812df82136aef08acb85e2159eae117f3e5
Kayla <keoliva@andrew.cmu.edu>
2019-03-03 17:07:59 -0500

    the result is now being determined using the answers

--- client/RadioForm.js
+++ client/RadioForm.js
@@ -1,60 +1,56 @@
-import React from "react";
-import Form from "./Form";
-import { popupUtil } from "./popup-util";
-
+import React from 'react';
+import Form from './Form';
+import { popupUtil } from './popup-util';
 
 class RadioForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      protip: null,
-      hoverShow: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleEnter = this.handleEnter.bind(this);
-    this.handleExit = this.handleExit.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			protip: null,
+			hoverShow: false,
+		};
+		this.handleEnter = this.handleEnter.bind(this);
+		this.handleExit = this.handleExit.bind(this);
+	}
 
-  handleClick(event) {
-    this.setState({ protip: event.target.value });
-  }
-  handleEnter(event) {
-    this.setState({ hoverShow: true });
+	handleEnter(event) {
+		this.setState({ hoverShow: true });
 
-    this.setState({ protip: popupUtil(event.target.value) });
-  }
-  handleExit(event) {
-    this.setState({ hoverShow: false });
-    this.setState({ protip: null });
-  }
+		this.setState({ protip: popupUtil(event.target.value) });
+	}
 
-  render() {
-    const name = this.props.question.name;
-    const choices = this.props.question.choices;
-    const handleSubmit = this.props.handleSubmit;
+	handleExit(event) {
+		this.setState({ hoverShow: false });
+		this.setState({ protip: null });
+	}
 
-    return (
-      <div className="questions-container">
-        {this.state.hoverShow ? <h4>{this.state.protip}</h4> : null}
-        <label>{name}</label>
-        <div className="choices-container">
-          {choices.map(choice => (
-            <button
-              type="button"
-              value={choice.label}
-              className="choice-buttons"
-              key={choice.value}
-              onClick={handleSubmit}
-              onMouseEnter={this.handleEnter}
-              onMouseOut={this.handleExit}
-            >
-              {choice.label}
-            </button>
-          ))}
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const name = this.props.question.name;
+		const choices = this.props.question.choices;
+		const handleSubmit = this.props.handleSubmit;
+
+		return (
+			<div className="questions-container">
+				{this.state.hoverShow ? <h4>{this.state.protip}</h4> : null}
+				<label>{name}</label>
+				<div className="choices-container">
+					{choices.map(choice => (
+						<button
+							type="button"
+							value={choice.label}
+							className="choice-buttons"
+							key={choice.id}
+							onClick={handleSubmit(choice.value)}
+							onMouseEnter={this.handleEnter}
+							onMouseOut={this.handleExit}
+						>
+							{choice.label}
+						</button>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Form(RadioForm);
