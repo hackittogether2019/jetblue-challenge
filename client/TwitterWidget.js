32c07f4eb6089d8c588cf1abc8125df995daa51e
Amanda G <amagonzalez27@gmail.com>
2019-03-03 15:15:24 -0500

    update twitter-widget

--- client/TwitterWidget.js
+++ client/TwitterWidget.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class TwitterWidget extends Component {
   render() {
     return (
-      <div className="twitter-widget">
+      <div className="twitter-widget text-light">
         <h4>Stay connected with us on Twitter</h4>
         <a
           href="https://twitter.com/jetblue?ref_src=twsrc%5Etfw"
-          className="twitter-follow-button twitter-follow"
+          className="twitter-follow-button"
           data-show-count="false"
         >
           Follow @jetblue
         </a>
-        <div />
+        <div style={{ height: '25px' }} />
         <a
-          className="twitter-timeline twitter-tl"
+          className="twitter-timeline"
           data-width="400"
           data-height="400"
           href="https://twitter.com/JetBlue?ref_src=twsrc%5Etfw"
