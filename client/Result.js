70f15b8d040ae5645938be37022ecde0a89cf957
Kayla <keoliva@andrew.cmu.edu>
2019-03-03 18:42:57 -0500

    book trip link

--- client/Result.js
+++ client/Result.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 class Result extends React.Component {
 	constructor() {
-		super()
-			this.state = {
-				imgUrl: "",
-				airport: ""
-			}
+		super();
+		this.state = {
+			imgUrl: '',
+			airport: '',
+		};
 	}
-	
+
 	render() {
 		const place = this.props.place;
 		return (
 			<div id="result">
-			{console.log(this.state.imgUrl)}
+				{console.log(this.state.imgUrl)}
 				<img src={`${this.state.imgUrl}`} />
 				<p>You should jet to...{place}!</p>
+				<a
+					href={`https://book.jetblue.com/B6/webqtrip.html?searchType=NORMAL&fareFamily=LOWESTFARE&origin=JFK&destination=${
+						this.state.airport
+					}&journeySpan=OW&source=GoogleFlights&referrerCode=GOOGLEFLIGHTS`}
+				>
+					Book your trip!
+				</a>
 			</div>
 		);
 	}
 
 	async componentDidMount() {
-		console.log(this.props.place)
-		const res = await axios.post('/api/destinations', {city: this.props.place})
-		console.log(res)
-    this.setState({ imgUrl: res.data.imgUrl, airport: res.data.airport })
+		console.log(this.props.place);
+		const res = await axios.post('/api/destinations', {
+			city: this.props.place,
+		});
+		console.log(res);
+		this.setState({ imgUrl: res.data.imgUrl, airport: res.data.airport });
 	}
 }
 
--- server/api/booking.js
+++ server/api/booking.js
@@ -3,29 +3,30 @@ const router = require('express').Router();
 const bestFareFinderURL = 'https://www.jetblue.com/best-fare-finder';
 
 router.get('/', (req, res) => {
-  try {
-    res.redirect(bestFareFinderURL);
-  } catch (error) {
-    console.log('Booking Page Not Found:', error);
-    res.sendStatus(404);
-  }
+	try {
+		res.redirect(bestFareFinderURL);
+	} catch (error) {
+		console.log('Booking Page Not Found:', error);
+		res.sendStatus(404);
+	}
 });
 
 router.post('/', (req, res) => {
-  const origin = req.body.origin;
-  const destination = req.body.destination;
-  try {
-    if (origin.length && destination.length) {
-      res.redirect(
-        `https://book.jetblue.com/B6/webqtrip.html?searchType=NORMAL&fareFamily=LOWESTFARE&origin=${origin}&destination=${destination}&journeySpan=OW&source=GoogleFlights&referrerCode=GOOGLEFLIGHTS`
-      );
-    } else {
-      res.redirect(bestFareFinderURL);
-    }
-  } catch (error) {
-    console.log('Posting trip failed: ', error);
-    res.sendStatus(404);
-  }
+	console.log(req.body);
+	const origin = req.body.origin;
+	const destination = req.body.destination;
+	try {
+		if (origin.length && destination.length) {
+			res.redirect(
+				`https://book.jetblue.com/B6/webqtrip.html?searchType=NORMAL&fareFamily=LOWESTFARE&origin=${origin}&destination=${destination}&journeySpan=OW&source=GoogleFlights&referrerCode=GOOGLEFLIGHTS`
+			);
+		} else {
+			res.redirect(bestFareFinderURL);
+		}
+	} catch (error) {
+		console.log('Posting trip failed: ', error);
+		res.sendStatus(404);
+	}
 });
 
 module.exports = router;
