85058d1889351dbdc6468a500c0b091bbb175044
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2019-03-03 15:01:35 -0500

    hot buttons yo

--- server/db/Choice.js
+++ server/db/Choice.js
@@ -1,15 +1,15 @@
-const Sequelize = require('sequelize')
-const db = require('./db')
+const Sequelize = require("sequelize");
+const db = require("./db");
 
-const Choice = db.define('choices', {
+const Choice = db.define("choices", {
   label: {
     type: Sequelize.STRING,
     allowNull: false
   },
   value: {
-    type: Sequelize.STRING,
+    type: Sequelize.ARRAY(Sequelize.STRING),
     allowNull: false
   }
-})
+});
 
-module.exports = Choice
+module.exports = Choice;
